aee6d14aa6f1f0c2d3d0afcdc0e41308750e312c
pedro.vinicius <pedro.vinicius@educ.rec.br>
2025-10-17 14:40:25 -0300

    Migrations with flyway

---
+++ rest-with-spring-boot-and-java/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person`
+VALUES
+(1,'Recife - Pernambuco - Brasil','Pedro','Male','Lapa'),
+(2,'Recife - Pernambuco - Brasil - F1','Ayton','Male','Senna'),
+(3,'Recife - Pernambuco - Brasil','Gustavo','Male','Carvalho');
